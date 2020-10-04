266a2f05e5606612f5cb64c340b8c9ca89185e69
Ishay Pozmantir <59173189+ishaypoz@users.noreply.github.com>
2020-10-05 00:20:58 +0300

    Add files via upload

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -10,6 +10,8 @@ import { ItemService } from '../../services/item.service';
 export class ItemsComponent implements OnInit {
   items: Item[];
   search: Item;
+  error: string = '';
+  info: string = '';
   @Input() id: number;
   constructor(private itemService: ItemService) {}
 
@@ -26,27 +28,44 @@ export class ItemsComponent implements OnInit {
   }
   deleteItem(item: Item) {
     this.itemService.deleteItem(item).subscribe((respone) => {
-      console.log(respone);
+      this.info = respone;
     });
     this.items = this.items.filter((i) => i.id !== item.id);
   }
   searchItem(id: number) {
-    this.itemService.getItem(id).subscribe((item) => {
-      this.items = [item];
-    });
+    this.itemService.getItem(id).subscribe(
+      (item) => {
+        this.error = '';
+        this.items = [item];
+      },
+      (err) => {
+        this.error = err.error;
+        this.items = [];
+      }
+    );
   }
   updateCount(data: any) {
     switch (data.option) {
       case 'withdraw':
         this.itemService.withdrawItem(data).subscribe(
-          () => this.searchItem(data.id),
-          (err) => console.log(err)
+          () => {
+            this.error = '';
+            this.searchItem(data.id);
+          },
+          (err) => {
+            this.error = err.error;
+          }
         );
         break;
       case 'deposit':
         this.itemService.depositItem(data).subscribe(
-          () => this.searchItem(data.id),
-          (err) => console.log(err)
+          () => {
+            this.error = '';
+            this.searchItem(data.id);
+          },
+          (err) => {
+            this.error = err.error;
+          }
         );
         break;
       default:
